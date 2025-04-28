4b5297ef7d8c7f9e4928c0cf9f2992c5c11df2c4
zhunan915 <zhunan@bu.edu>
2025-04-27 21:21:44 -0700

    test

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_creator_role_arn" {
-  description = "ARN of the created VPC Creator Role"
-  value       = aws_iam_role.vpc_creator_role.arn
-}
-
-output "vpc_creator_role_name" {
-  description = "Name of the created VPC Creator Role"
-  value       = aws_iam_role.vpc_creator_role.name
-}
